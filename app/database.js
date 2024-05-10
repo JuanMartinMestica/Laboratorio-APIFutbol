897e5715adbe56413403f3145658d5c42012f845
Juan Martín Mestica <juan.mestica@est.fi.uncoma.edu.ar>
2024-05-10 08:52:08 -0300

    Se configura API para deploy en Render

--- app/database.js
+++ app/database.js
@@ -1,11 +1,8 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.HOST,
-  database: process.env.DATABASE,
-  password: process.env.PASSWORD,
-  port: process.env.DB_PORT
+  connectionString: process.env.DB_URL,
+  ssl: true,
 });
 
 module.exports = {
